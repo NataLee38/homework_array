8f8e2d00ca931d7b0f2e0cbb6fea90c29d407b44
NataLee38 <light3475house@gmail.com>
2021-07-28 14:22:36 +0300

    first try

---
+++ script.js
@@ -0,0 +1,18 @@
+// let arr = [1, 2, 3, 4, 5,];
+//
+// console.log(arr.slice(0, 3));
+// console.log(arr.slice(3, 6));
+
+const num = "1124356712453";
+
+function spliceNum (num, symbol) {
+    let arr = [];
+    return num.split("");
+    let filtered = num.filter((item) => {
+        if (item % 2 !== 0) {
+
+        }
+    })
+}
+
+console.log(spliceNum(num, "-"));
