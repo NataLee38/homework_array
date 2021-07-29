4662f2d26b4ea2c5bc1b2b1084d17cc9e79e911b
NataLee38 <light3475house@gmail.com>
2021-07-30 01:41:52 +0300

    /

--- script.js
+++ script.js
@@ -3,16 +3,19 @@
 // console.log(arr.slice(0, 3));
 // console.log(arr.slice(3, 6));
 
-const num = "1124356712453";
+let str = "1124356712453";
 
-function spliceNum (num, symbol) {
-    let arr = [];
-    return num.split("");
-    let filtered = num.filter((item) => {
-        if (item % 2 !== 0) {
+function splitEven (str, symbol) {
+    let splitted = str.split('');
 
+    return splitted.map((item) => {
+        const isOdd = item % 2 === 0;
+        if (isOdd) {
+            return item;
         }
-    })
+
+        return item + symbol;
+    }).join('');
 }
 
-console.log(spliceNum(num, "-"));
+console.log(splitEven(str, "-"));
